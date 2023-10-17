6a571464d4c9928a64cb4e4503ff46cc058ef3b9
Vlad Stelmakh <vlad1k9369@gmail.com>
2023-10-17 15:49:09 +0200

    Create bin search algorithm

---
+++ bin_search/bin_search.py
@@ -0,0 +1,18 @@
+def bin_search(arr, target):
+    left_idx = 0
+    right_idx = len(arr) - 1
+
+    while left_idx < right_idx:
+        mid_idx = (int) ((left_idx + right_idx) / 2)
+
+        if (target == arr[mid_idx]):
+            return mid_idx
+
+        if (target > arr[mid_idx]):
+            left_idx = mid_idx
+        else: 
+            right_idx = mid_idx
+
+    return None
+
+    
