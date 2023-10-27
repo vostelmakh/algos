ce4cc1f24a226055cc769b504ea43b190a9a0a2b
Vlad Stelmakh <vlad1k9369@gmail.com>
2023-10-27 17:47:05 +0200

    max_ones_length

--- max_ones_length/max_ones_length.py
+++ max_ones_length/max_ones_length.py
@@ -1,7 +1,5 @@
 # Дан массив из нулей и единиц. Нужно определить, какой максимальный по длине подинтервал единиц можно получить, удалив ровно один элемент массива.
 
-# [1, 1, 0]
-
 def max_ones_length(seq):
     groups = []
 
@@ -13,11 +11,35 @@ def max_ones_length(seq):
                 groups.append(0)
             else:
                 groups.append(count)
-                groups.append(0)
-            
             
             count = 0
         else:
             count += 1
 
-    return groups
+    if (count > 0):
+        groups.append(count)
+
+    if (len(groups) == 0):
+        return 0 
+
+    max_seq = groups[0]
+
+    max_index = len(groups) - 1
+
+    for index in range(len(groups) - 1):
+        prevIndex = index - 1
+        nextIndex = index + 1
+
+        if (prevIndex > 0):
+            val = groups[prevIndex] + groups[index]
+            max_seq = max(val, max_seq)
+        
+        if (nextIndex <= max_index):
+            val = groups[nextIndex] + groups[index]
+            max_seq = max(val, max_seq)
+
+        if (prevIndex > 0 and nextIndex <= max_index):
+            val = groups[nextIndex] + groups[prevIndex]
+            max_seq = max(val, max_seq)
+
+    return max_seq
--- max_ones_length/test.py
+++ max_ones_length/test.py
@@ -3,8 +3,32 @@ from max_ones_length import max_ones_length
 
 class Test(unittest.TestCase):
     def test_case1(self):
-        result = max_ones_length([0, 0, 1, 1, 0, 1, 1, 0])
-        self.assertCountEqual(result, 4)
+        result = max_ones_length( [0, 0, 1, 1, 0, 1, 1, 0] )
+        self.assertEquals(result, 4)
+
+    def test_case2(self):
+        result = max_ones_length( [1, 1, 0] )
+        self.assertEquals(result, 2)
+
+    def test_case3(self):
+        result = max_ones_length( [1, 0, 1, 1, 0] )
+        self.assertEquals(result, 3)
+
+    def test_case4(self):
+        result = max_ones_length( [0, 1, 1, 0, 1, 1, 1, 0, 1, 1] )
+        self.assertEquals(result, 5)
+
+    def test_case5(self):
+        result = max_ones_length( [1, 1, 1, 1, 1] )
+        self.assertEquals(result, 5)
+
+    def test_case6(self):
+        result = max_ones_length( [0, 0, 0, 0, 0] )
+        self.assertEquals(result, 0)
+
+    def test_case7(self):
+        result = max_ones_length( [] )
+        self.assertEquals(result, 0)
 
 if __name__ == '__main__':
     unittest.main()
